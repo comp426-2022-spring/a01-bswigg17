8ca5e3e441c41e9e40d6dbeada5c4db980019b25
Seth Wiggins <bswigg17@gmail.com>
2022-01-27 16:21:03 -0500

    Fix

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs'); 
+const { exit } = require('process');
 const args = require('minimist')(process.argv.slice(2));
 
 const port = args['port'] || 3000; 
@@ -7,19 +8,16 @@ const port = args['port'] || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200; 
     res.setHeader('Content-Type', 'text/html');
-    fs.readFile('./www/index.html', (err, data) => {
-        if (err) {
-            console.log(err);
-            return
-        }
-        res.end(data); 
-    }); 
-}); 
 
-process.on('uncaughtException', err => {
-    console.error('Error Was Not Caught', err);
-    process.exit(1); 
-});
+    const data = fs.readFileSync('./www/index2.html', 'utf8'); 
+    if (data) {
+        res.end(data); 
+    }
+    else {
+        console.log(err);
+        exit(1); 
+    }
+}); 
 
 server.listen(port, () => {
     console.log(`Server running at port ${port}`); 
