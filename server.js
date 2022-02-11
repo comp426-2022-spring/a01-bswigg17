c3e7722aba8a804d52a8ca22fe58e3303e618db8
Seth Wiggins <bswigg17@gmail.com>
2022-02-11 09:54:40 -0500

    Fix

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const server = http.createServer((req, res) => {
     fs.readFile('./www/index.html', 'utf8', (err, data) => {
         if (err) {
             console.log(err);
-            process.exit(1)
             return 
         }
         res.end(data); 
